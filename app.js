cef524664de13ce06ab7bf875aa75beb71b3dbe3
Eduardo Sandi <dudaesouza@gmail.com>
2024-11-27 00:33:56 -0300

    .

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const apiKey = '8175fA5f6098c5301022f475da32a2aa';
 let token = '';
 let currentPage = 1;
 const pageSize = 12;
+const totalRecords = 105;
+const totalPages = Math.ceil(totalRecords / pageSize);
 let isLoading = false;
 let isEndOfData = false;
 
@@ -9,27 +11,26 @@ const loadingElement = document.getElementById('loading');
 const imageGallery = document.getElementById('image-gallery');
 
 document.addEventListener('DOMContentLoaded', async () => {
-    await authenticate();
+    await autenticar();
 
     if (!token) {
-        console.error('Token não foi obtido. Verifique a autenticação.');
+        console.error('Sem Token');
         return;
     }
-    loadImages();
+    carregarImagens();
     window.onscroll = () => {
         if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && !isLoading && !isEndOfData) {
             currentPage++;
-            loadImages();
+            carregarImagens();
         }
     };
 });
 
-async function authenticate() {
+async function autenticar() {
     try {
         const response = await fetch('https://ucsdiscosapi.azurewebsites.net/Discos/autenticar', {
             method: 'POST',
-            headers: {
-                'accept': '*/*',                
+            headers: {        
                 'ChaveApi': apiKey,
             }
         });
@@ -45,20 +46,25 @@ async function authenticate() {
     }
 }
 
-async function loadImages() {
+async function carregarImagens() {
     isLoading = true;
     toggleLoading(true);
+
+
+    if (currentPage >= totalPages) {
+        currentPage = 1;
+      }
+
     const url = `https://ucsdiscosapi.azurewebsites.net/Discos/records?numeroInicio=${(currentPage - 1) * pageSize + 1}&quantidade=${pageSize}`;
     try {
         const response = await fetch(url, {
             method: 'GET',
             headers: {
-                'accept': '*/*',
                 'TokenApiUCS': token,
             }
         });
         const data = await response.json();
-        renderImages(data);
+        renderizarImagens(data);
     } catch (error) {
         console.error('Erro ao carregar imagens:', error);
     } finally {
@@ -70,20 +76,18 @@ function toggleLoading(isVisible) {
     loadingElement.style.display = isVisible ? 'block' : 'none';
 }
 
-function renderImages(images) {
+
+function renderizarImagens(images) {
     images.forEach(image => {
         const col = document.createElement('div');
-        col.classList.add('col-6', 'col-md-4', 'mb-4');
-        
+        col.classList.add('col-12', 'col-md-6', 'mb-3');
         const card = document.createElement('div');
         card.classList.add('card', 'image-card');
-        card.onclick = () => showModal(image);
-
+        card.onclick = () => mostrarModal(image);
         const img = document.createElement('img');
         img.classList.add('card-img-top');
         img.src = `data:image/jpeg;base64,${image.imagemEmBase64}`; 
         img.alt = image.descricaoPrimaria; 
-
         card.appendChild(img);
         col.appendChild(card);
         imageGallery.appendChild(col);
@@ -91,15 +95,13 @@ function renderImages(images) {
 }
 
 
-function showModal(image) {
+function mostrarModal(image) {
     const modalTitle = document.getElementById('albumModalLabel');
     const modalImage = document.getElementById('modal-image');
     const modalDescription = document.getElementById('modal-description');
-
     modalTitle.innerText = image.descricaoPrimaria;
     modalImage.src = `data:image/jpeg;base64,${image.imagemEmBase64}`;
     modalDescription.innerText = image.descricaoSecundaria;
-
     const modal = new bootstrap.Modal(document.getElementById('albumModal'));
     modal.show();
 }
